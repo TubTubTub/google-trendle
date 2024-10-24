b830051fc3056577236d6e3e42a2a8c7a343a4ac
TubTubTub <tobykwmok@gmail.com>
2024-10-24 11:28:14 +0100

    finished leaderboard frontend

--- frontend/src/services/rankings.js
+++ frontend/src/services/rankings.js
@@ -5,7 +5,10 @@ const baseURL = `${BACKEND_URL}/api/rankings`
 
 const getAll = async () => {
     // const result = await axios.get(baseURL)
-    return [['Deez nuts', 100], ['Jeremiah', 69], ['Derke', 10]]
+    return [['Deez nuts', 100], ['Jeremiah', 69], ['Derke', 10], ['Greg', 12], ['JJ Olatunji', 232], ['Bean', 3],
+            ['Deez nuts', 100], ['Jeremiah', 69], ['Derke', 10], ['Greg', 12], ['JJ Olatunji', 232], ['Bean', 3],
+            ['Deez nuts', 100], ['Jeremiah', 69], ['Derke', 10], ['Greg', 12], ['JJ Olatunji', 232], ['Bean', 3],
+            ['Deez nuts', 100], ['Jeremiah', 69], ['Derke', 10], ['Greg', 12], ['JJ Olatunji', 232], ['Bean', 3]]
 }
 
 export default { getAll }
